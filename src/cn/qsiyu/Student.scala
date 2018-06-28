c2bad9a6142a512957b318871fc56c621886df42
QYuTian <327950431@qq.com>
2018-06-28 10:07:44 +0800

    "created git TestGitJava.java"

--- src/cn/qsiyu/Student.scala
+++ src/cn/qsiyu/Student.scala
@@ -5,10 +5,20 @@ package cn.qsiyu
   * WeChat: jianlai2008
   * created: 2018/6/28 8:50
   */
-class Student {
-  def learn() :Unit ={
+class Student(val name: String, var age: Int) {
+  var gender: Int = _
 
-    println()
+  def this(name: String, age: Int, gender: Int) {
+    this(name, age)
+    this.gender = gender
+  }
+
+  def learn(): String = {
+
+    println(this.name + "learn")
+    name
 
   }
+
+  override def toString = s"Student($gender, $name, $age)"
 }
---
+++ src/cn/qsiyu/TestGitJava.java
@@ -0,0 +1,9 @@
+package cn.qsiyu;
+
+/**
+ * User: QYT
+ * WeChat: jianlai2008
+ * created: 2018/6/28 10:06
+ */
+public class TestGitJava {
+}
